e2fb3c78da378effa7235ba4fbc3f076ef023a06
minimo <minimo.mobile@gmail.com>
2017-10-30 23:55:35 +0900

    Update.

--- KumaBang_Unity/Assets/Scripts/ActorNameController.cs
+++ KumaBang_Unity/Assets/Scripts/ActorNameController.cs
@@ -13,6 +13,11 @@ public class ActorNameController : MonoBehaviour {
 
     //移動処理
     public void flick(bool isRight, float delay = 0.0f) {
+        StartCoroutine(flickCoroutine(isRight));
+        return;
+    }
+
+    private IEnumerator flickCoroutine(bool isRight) {
         float move = 10.0f;
         if (isRight) move *= -1.0f;
         iTween.MoveBy(this.gameObject,
@@ -20,9 +25,19 @@ public class ActorNameController : MonoBehaviour {
                 "x", move,
                 "easeType", iTween.EaseType.easeOutQuint,
                 "time", 1.0f,
-                "delay", delay + 0.1f,
-                "oncomplete", "OnCompleteCallback",
-                "oncompletetarget", this.gameObject
+                "delay", 0.1f
             ));
+        yield return new WaitForSeconds(1.1f);
+
+        Vector3 pos = this.transform.position;
+        pos.x = isRight? 10.0f: -10.0f;
+        this.transform.position = pos;
+        iTween.MoveBy(this.gameObject,
+            iTween.Hash(
+                "x", move,
+                "easeType", iTween.EaseType.easeOutQuint,
+                "time", 1.0f
+            ));
+        
     }
 }
