08304b9fcaf43be4a9d0aa68a251c372687d556e
minimo <minimo.mobile@gmail.com>
2017-12-04 01:16:11 +0900

    Update.

---
+++ KumaBang_Unity/Assets/#ProjectName/000_Common/Scripts/MaskSpriteController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MaskSpriteController : MonoBehaviour {
+
+    [SerializeField] Vector3 size;
+
+	// Use this for initialization
+	void Start () {
+        this.RotateIn(10.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(0.0f, 0.0f, 3.0f);
+	}
+
+    public void RotateIn(float time = 1.0f) {
+        this.transform.localScale = Vector3.zero;
+        this.transform.DOScale(20.0f, time);
+    }
+}
--- KumaBang_Unity/Assets/#ProjectName/010_Title/Scripts/TitleTextController.cs
+++ KumaBang_Unity/Assets/#ProjectName/010_Title/Scripts/TitleTextController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TitleTextController : MonoBehaviour {
 
@@ -8,6 +9,19 @@ public class TitleTextController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        //出現アニメーション
+        switch (this.index) {
+            case 0:
+                this.transform.DOLocalMove(new Vector3(150.0f, 0.0f, 0.0f), 3.0f)
+                    .SetEase(Ease.OutBounce)
+                    .SetRelative();
+                break;
+            case 1:
+                this.transform.DOLocalMove(new Vector3(-150.0f, 0.0f, 0.0f), 3.0f)
+                    .SetEase(Ease.OutBounce)
+                    .SetRelative();
+                break;
+        }
 	}
 	
 	// Update is called once per frame
