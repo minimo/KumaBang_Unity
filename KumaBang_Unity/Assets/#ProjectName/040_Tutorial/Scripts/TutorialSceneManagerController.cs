c16669663ffe0b6f660e885974dc12ca2b49cd85
minimo <minimo.mobile@gmail.com>
2018-01-23 00:32:37 +0900

    Update.

---
+++ KumaBang_Unity/Assets/#ProjectName/040_Tutorial/Scripts/TutorialSceneManagerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSceneManagerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int numStage = ApplicationManagerController.Instance.playingStageNumber;
+
+        //チュートリアル準備
+        string name = "Prefabs/Tutorial" + numStage;
+        GameObject tutorial1 = Instantiate((GameObject)Resources.Load(name + "_1"));
+        tutorial1.transform.SetParent(this.transform);
+        GameObject tutorial2 = Instantiate((GameObject)Resources.Load(name + "_1"));
+        tutorial2.transform.SetParent(this.transform);
+	}
+}
